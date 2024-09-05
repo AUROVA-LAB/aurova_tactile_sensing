48e0d3f9b76767a967926d805a4ad6a252f1aaa7
Julio Castano <jcarobotica@gmail.com>
2024-09-05 13:09:56 +0200

    solved issue with callback in estimate_force.py

--- force_estimation/scripts/estimate_force.py
+++ force_estimation/scripts/estimate_force.py
@@ -2,7 +2,8 @@
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
-from cv_bridge import CvBridge
+
+import ros_numpy
 
 import cv2
 import os
@@ -23,49 +24,45 @@ NUM_WORKERS = os.cpu_count()
 PIN_MEMORY = True
 
 digit_image = None
-gt_force = None
 
 
-bridge = CvBridge()
-
-
-def publish_force_value(model, test_transform, pub_force):
+def publish_force_value(digit_image, model, test_transform, pub_force):
 	'''
 	Publish the estimated force value in N from the RGB image.
 	'''
-	
-	global digit_image, gt_force
 
-	while not rospy.is_shutdown():
+	if digit_image is not None:
 
-		if digit_image is not None:
+		frame = digit_image
+		frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-			frame = digit_image
-			frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		preprocess_img = test_transform(frame_rgb)
 
-			preprocess_img = test_transform(frame_rgb)
+		preprocess_img_cuda = preprocess_img.unsqueeze(0).to(DEVICE)
+		predicted_force = model(preprocess_img_cuda) 
 
-			preprocess_img_cuda = preprocess_img.unsqueeze(0).to(DEVICE)
-			predicted_force = model(preprocess_img_cuda) 
-
-			post_pred_force = predicted_force.detach().cpu().numpy().squeeze()
-			
-			pub_force.publish(post_pred_force)
+		post_pred_force = predicted_force.detach().cpu().numpy().squeeze()
+		
+		pub_force.publish(post_pred_force)
 
 
 
-def callback(img):
+def callback(img, args):
 	
 	global digit_image
 
-	digit_image = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
+	model = args[0]
+	test_transform = args[1]
+	pub_force = args[2]
+
+	digit_image = ros_numpy.numpify(img)
+
+	publish_force_value(digit_image, model, test_transform, pub_force)
 
 
 def main():
 
 	rospy.init_node('force_estimation', anonymous=True)
-
-	digit_img_sub = rospy.Subscriber("digit55/camera/image_color", Image, callback)
 
 	pub_force = rospy.Publisher(f"digit/force_value", Float32, queue_size=10)
 
@@ -87,7 +84,9 @@ def main():
 		],
 	)
 
-	publish_force_value(model, test_transform, pub_force)
+	digit_img_sub = rospy.Subscriber("digit55/camera/image_color", Image, callback, (model, test_transform, pub_force))
+
+	rospy.spin()
 	
 	
 
